16bee18f1d810212b23d0078b696af44af0fccd4
gxguinho <gabrielapl@unipam.edu.br>
2023-09-11 19:23:55 -0300

    add components

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,13 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+  args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
+} as Meta
+
+export const Primary: StoryObj<BoxProps> = {}
