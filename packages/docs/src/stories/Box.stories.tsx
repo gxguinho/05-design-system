c600d7091241b8fa053cb249d134c3537037b456
gxguinho <gabrielapl@unipam.edu.br>
2023-09-11 22:40:18 -0300

    add Toast

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -8,6 +8,13 @@ export default {
   args: {
     children: <Text>Testando o elemento Box</Text>,
   },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
 } as Meta
 
 export const Primary: StoryObj<BoxProps> = {}
